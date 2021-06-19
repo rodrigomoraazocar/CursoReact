232f98451d9f4e3c0fddc43b5cddd556bc280a16
Rodrigo A. Mora Azócar <brazuk25@gmail.com>
2021-06-19 16:47:12 -0300

    Meu Primeiro Componente

---
+++ src/indeex.js
@@ -0,0 +1,27 @@
+
+//Importar Modulos
+import React  from 'react';
+import ReactDOM from 'react-dom';
+
+
+//Componente
+
+class MeuComponente extends React.Component {
+
+render (){
+
+   return <h1>Meu Componente</h1> 
+
+}
+
+}
+
+
+//Render
+
+ReactDOM.render(<MeuComponente />, document.getElementById('root'));
+
+
+
+
+
